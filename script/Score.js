bb4db24ab7e33638d60194f9449715a6ffacce9d
Fuerederico <fuerederico0909@gmail.com>
2025-06-10 06:50:00 +0900

    Score.js の更新

--- script/Score.js
+++ script/Score.js
@@ -1,15 +1,22 @@
 export class Score {
-  constructor(name, level, n_count, arrayData) {
+  constructor(name, level, n_cnt, noteData) {
     this.name = name;
     this.level = level;
-    this.n_count = n_count;
+    this.n_cnt = n_cnt;
+    this.noteData = noteData;
 
-    this.type = arrayData.map(d => d.type);
-    this.c_bns = arrayData.map(d => d.c_bns);
-    this.actskl1 = arrayData.map(d => d.actskl1);
-    this.actskl2 = arrayData.map(d => d.actskl2);
+    this.type = noteData.map(d => d.type);
+    this.c_bns = noteData.map(d => d.c_bns);
+    this.actskl1 = noteData.map(d => d.actskl1);
+    this.actskl2 = noteData.map(d => d.actskl2);
   }
 
+  calculate(total, selectedValue) {
+    // 仮の計算式なのだ（あとで書き換えるのだ）
+    return this.noteData.reduce((sum, d) => sum + (total + selectedValue) * d.c_bns, 0);
+  }
+
+/*
   calculate(total, selectedValue) {
     let sum = 0;
     for (let i = 0; i < this.n_count; i++) {
@@ -17,4 +24,6 @@ export class Score {
     }
     return sum;
   }
+*/
+
 }

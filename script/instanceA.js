ea718ba66c4c046356402ef80790289d162ce157
Fuerederico <fuerederico0909@gmail.com>
2025-06-11 23:39:42 +0900

    Update instanceA.js

--- script/instanceA.js
+++ script/instanceA.js
@@ -1,7 +1,16 @@
 import { Score } from './Score.js';
 
 export async function loadInstanceA() {
-  const response = await fetch('/score-calculation/data/A_array.json');
+  let path = "";
+  if (location.origin.includes("github.io")) {
+    // GitHub Pages 用
+    path = "/score-calculation/data/A_array.json";
+  } else {
+    // ローカル用（Live Serverなど）
+    path = "./data/A_array.json";
+  }
+
+  const response = await fetch(path);
   const arrayData = await response.json();
   return new Score("A", 22, 544, arrayData);
 }
